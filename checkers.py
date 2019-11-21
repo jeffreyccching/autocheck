2f40944716df7d235454864eec040924b2ef7584
AbelArmede <a.g.armede@gmail.com>
2019-11-20 23:52:13 -0500

    Updated the state representation

--- checkers.py
+++ checkers.py
@@ -3,27 +3,34 @@ import numpy as np
 
 def state_from_board(board):
     """
-    The state takes board as an input
+    The state_from_board takes board as an input
         board is a 2-D numpy array where
-         -1 is an invalid space
          0 is an empty space
          1 is a Black Solider
          2 is a White Solider
          3 is a Black King
          4 is a White King
     The state returns a 3-D array that in which
-        [][][0]  1 if Black Solider or 0
-        [][][1]  1 if White Solider or 0
-        [][][2]  1 if Black King or 0
-        [][][3]  1 if White King or 0
+        [0][][]  1 if Black Solider or 0
+        [1][][]  1 if White Solider or 0
+        [2][][]  1 if Black King or 0
+        [3][][]  1 if White King or 0
     """
-    nn_board = np.zeros((board[0].size, board[:, 0].size, 4), dtype=np.int16)
-    nn_board[3, :, :] = board/4
-    nn_board[2, :, :] = board / 3 - nn_board[3, :, :]
-    nn_board[1, :, :] = board / 2 - nn_board[2, :, :] - 2*nn_board[3, :, :]
-    nn_board[0, :, :] = board - 2*nn_board[1, :, :] - 3*nn_board[2, :, :] - 4*nn_board[3, :, :]
+    state = np.zeros((board[0].size, board[:, 0].size, 4), dtype=np.int16)
+    state[3, :, :] = board/4
+    state[2, :, :] = board / 3 - state[3, :, :]
+    state[1, :, :] = board / 2 - state[2, :, :] - 2*state[3, :, :]
+    state[0, :, :] = board - 2*state[1, :, :] - 3*state[2, :, :] - 4*state[3, :, :]
 
-    return nn_board
+    return state
+
+
+def board_from_state(state):
+    """
+    It does the inverse of state_from_board
+    """
+    board = 4*state[3, :, :] + 3*state[2, :, :] + 2*state[1, :, :] + state[0, :, :]
+    return board
 
 
 def expand_board(board):
@@ -52,6 +59,7 @@ if __name__ == "__main__":
     print(board)
     a = state_from_board(board)
     b = expand_board(board)
+    c = board_from_state(a)
     print(a)
     print(b)
-    print( "This should be a checkers AI, but it isn't one." )
+    print(c)
